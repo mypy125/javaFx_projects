42ba8d1d32ded88b07ff2c83ec60cd12d5f83ecf
mohito <mypywelcome@gmail.com>
2024-06-19 14:59:28 +0400

    Sample_Disk_Analizer

---
+++ src/main/java/com/mygitgor/Starter.java
@@ -0,0 +1,16 @@
+package com.mygitgor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
